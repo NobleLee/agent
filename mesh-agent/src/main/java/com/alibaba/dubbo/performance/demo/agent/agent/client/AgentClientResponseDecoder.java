fa3e1a0f4c8a523d1ef5d9b70be43c19c812bfee
gaoguili <385460398@qq.com>
2018-05-07 23:04:13 +0800

    统一版本

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/agent/client/AgentClientResponseDecoder.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/agent/client/AgentClientResponseDecoder.java
@@ -6,6 +6,7 @@ import com.alibaba.dubbo.performance.demo.agent.dubbo.model.RpcRequestHolder;
 import com.alibaba.dubbo.performance.demo.agent.dubbo.model.RpcResponse;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.ByteToMessageDecoder;
 
 import java.util.Arrays;
@@ -18,30 +19,39 @@ import java.util.List;
  * @author gaoguili
  * @create 2018-05-07 下午4:00
  */
-public class AgentClientResponseDecoder extends ByteToMessageDecoder {
+public class AgentClientResponseDecoder extends ChannelInboundHandlerAdapter {
 
 
     private RpcRequestHolder<RpcFuture> requestHolder = RpcRequestHolder.getRpcRequestHolderByName("agentClient");
 
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            byte[] rev = new byte[in.readableBytes()];
 
-        byte[] data = new byte[in.readableBytes()];
-        in.readBytes(data);
+            in.readBytes(rev);
+            System.err.println(rev.length + Arrays.toString(rev));
 
-        byte[] requestIdBytes = Arrays.copyOfRange(data, 0, 8);
-        long requestId = Bytes.bytes2long(requestIdBytes, 0);
-        byte[] subArray = Arrays.copyOfRange(data, 0, 8);
 
-        RpcResponse response = new RpcResponse();
-        response.setBytes(subArray);
-        response.setRequestId("" + requestId);
+            byte[] requestIdBytes = Arrays.copyOfRange(rev, 0, 8);
+            long requestId = Bytes.bytes2long(requestIdBytes, 0);
+            byte[] subArray = Arrays.copyOfRange(rev, 8, rev.length);
 
-        RpcFuture future = requestHolder.get("" + requestId);
-        if (null != future) {
-            requestHolder.remove("" + requestId);
-            future.done(response);
+            RpcResponse response = new RpcResponse();
+            response.setBytes(subArray);
+            response.setRequestId(String.valueOf(requestId));
+
+            System.err.println(Arrays.toString(response.getRequestId().getBytes()) + Arrays.toString(response.getBytes()));
+
+            RpcFuture future = requestHolder.get(response.getRequestId());
+            if (null != future) {
+                requestHolder.remove(response.getRequestId());
+                future.done(response);
+            }
+        } finally {
+            in.release();
         }
-        in.release();
+
     }
 }
