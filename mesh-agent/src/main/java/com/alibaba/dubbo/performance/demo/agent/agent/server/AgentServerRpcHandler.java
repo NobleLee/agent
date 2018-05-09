c8a3f0e5089ca6034315709a26adf5b990c79b63
gaoguili <385460398@qq.com>
2018-05-09 21:12:46 +0800

    2.0 版本QPS测试1300左右，但是provider线程启动太多，引起GC严重，因此负载均衡算法尤为重要

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/agent/server/AgentServerRpcHandler.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/agent/server/AgentServerRpcHandler.java
@@ -1,13 +1,10 @@
 package com.alibaba.dubbo.performance.demo.agent.agent.server;
 
-import com.alibaba.dubbo.performance.demo.agent.agent.COMMON;
 import com.alibaba.dubbo.performance.demo.agent.dubbo.RpcClient;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-
-import java.nio.charset.Charset;
 
 /**
  * 描述:
@@ -24,18 +21,7 @@ public class AgentServerRpcHandler extends SimpleChannelInboundHandler<ByteBuf> 
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
-        // TCP 拆包问题  //将ctx上下文放到一个map中，最后根据id，返回给相应的客户端
-//        String message = msg.toString(Charset.defaultCharset());
-//
-//
-//        String[] msgs = message.split("\\" + COMMON.AttributeSeparator);
-//        if (msgs.length == 4) {
-//            String[] copy = new String[5];
-//            for (int i = 0; i < 4; i++)
-//                copy[i] = msgs[i];
-//            copy[4] = "";
-//            msgs = copy;
-//        }
+
         if (msg.readableBytes() < 9) return;
         rpcClient.sendDubbo(msg);
     }
