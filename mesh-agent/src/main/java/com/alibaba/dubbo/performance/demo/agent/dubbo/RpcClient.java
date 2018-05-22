d5b1f6d92ae704295086bbd8d757f3a0cc899803
gaoguili <385460398@qq.com>
2018-05-22 11:00:06 +0800

    更改back-log

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/dubbo/RpcClient.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/dubbo/RpcClient.java
@@ -1,19 +1,23 @@
 package com.alibaba.dubbo.performance.demo.agent.dubbo;
 
 import com.alibaba.dubbo.performance.demo.agent.agent.COMMON;
+import com.alibaba.dubbo.performance.demo.agent.agent.server.AgentServerRpcHandler;
 import com.alibaba.dubbo.performance.demo.agent.dubbo.model.DubboRequest;
 import com.alibaba.dubbo.performance.demo.agent.dubbo.model.RpcInvocation;
 import com.alibaba.dubbo.performance.demo.agent.tools.ByteBufUtils;
 import com.alibaba.dubbo.performance.demo.agent.tools.JsonUtils;
 import com.google.common.base.Charsets;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.epoll.EpollSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.http.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,6 +25,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
 
 /**
  * 描述: Dubbo 和 agent 的调用RPCß
@@ -42,7 +52,7 @@ public class RpcClient {
             protected void initChannel(EpollSocketChannel ch) {
                 ChannelPipeline pipeline = ch.pipeline();
                 pipeline.addLast(new DelimiterBasedFrameDecoder(2048, delimiter));
-               // pipeline.addLast(new DubboRpcEncoder());
+                // pipeline.addLast(new DubboRpcEncoder());
                 pipeline.addLast(new DubboRpcDecoder());
             }
         });
@@ -112,9 +122,10 @@ public class RpcClient {
 
     /**
      *  直接将请求发送Dubbo
+     *
      * @param buf
      */
-    public void sendDubboDirect(ByteBuf buf){
+    public void sendDubboDirect(ByteBuf buf) {
         Channel channel = null;
         try {
             channel = connectManager.getChannel();
@@ -122,8 +133,28 @@ public class RpcClient {
             e.printStackTrace();
         }
         ByteBuf byteBuf = DubboRpcEncoder.directSend(buf);
-       // ByteBufUtils.printStringln(byteBuf,16,"");
+        // ByteBufUtils.printStringln(byteBuf,16,"");
         channel.writeAndFlush(byteBuf);
+    }
+
+
+    /**
+     * 测试直接将结果返回
+     *
+     * @param buf
+     * @param channel
+     */
+    private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(256);
+
+    public void sendBackTest(ByteBuf buf) {
+        long reqid = buf.readLong();
+        byte[] hashcode = String.valueOf(buf.toString(Charsets.UTF_8).hashCode()).getBytes();
+        ByteBuf buffer = PooledByteBufAllocator.DEFAULT.directBuffer(hashcode.length + 8);
+        buffer.writeLong(reqid);
+        buffer.writeBytes(hashcode);
+        executorService.schedule(() -> {
+            AgentServerRpcHandler.channel.writeAndFlush(buffer);
+        }, 50, TimeUnit.MILLISECONDS);
     }
 
 }
