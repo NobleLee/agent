ab55f288d8304ecc428c01660484d60b9ca95f0c
gaoguili <385460398@qq.com>
2018-05-24 16:17:42 +0800

    增加创建连接选项

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/dubbo/DubboRpcDecoder.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/dubbo/DubboRpcDecoder.java
@@ -7,31 +7,16 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
 
 
-public class DubboRpcDecoder extends ChannelInboundHandlerAdapter {
+public class DubboRpcDecoder extends ByteToMessageDecoder {
 
 
-    protected final static byte[] MAGIC = new byte[]{-38, -69};
-
-    // 接收dubbo消息，并将消息传送给client
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ByteBuf byteBuf = (ByteBuf) msg;
-        try {
-            if (byteBuf.readableBytes() < 6) return;
-            int readlength = byteBuf.readableBytes();
-            ByteBuf buffer = PooledByteBufAllocator.DEFAULT.directBuffer(readlength - 7);
-            buffer.writeBytes(MAGIC);
-            buffer.writeBytes(byteBuf, 2, 8);
-            buffer.writeBytes(byteBuf, 16, readlength - 17);
-            int index = (int) (byteBuf.getLong(2) & 0x7);
-            AgentServerRpcHandler.channels.get(index).writeAndFlush(buffer);
-        } finally {
-            byteBuf.release();
-        }
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
 
     }
-
-
 }
