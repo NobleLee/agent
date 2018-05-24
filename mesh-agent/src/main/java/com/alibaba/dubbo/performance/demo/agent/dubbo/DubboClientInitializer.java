2134ea763dfc84d56dabf20b844bfca0b5eeb201
qiuxiaochen <1015970848@qq.com>
2018-05-24 17:01:29 +0800

    Merge branch 'agent_netty_dev_nio' of github.com:HelloWorldTestM/agent into agent_netty_dev_nio
    # Conflicts:
    #	mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/agent/COMMON.java

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/dubbo/DubboClientInitializer.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/dubbo/DubboClientInitializer.java
@@ -1,6 +1,8 @@
 package com.alibaba.dubbo.performance.demo.agent.dubbo;
 
 import com.alibaba.dubbo.performance.demo.agent.agent.COMMON;
+import com.alibaba.dubbo.performance.demo.agent.dubbo.LoadBalance.MyInBoundHandler;
+import com.alibaba.dubbo.performance.demo.agent.dubbo.LoadBalance.MyOutBoundHandler;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelInitializer;
@@ -21,8 +23,14 @@ public class DubboClientInitializer extends ChannelInitializer<NioSocketChannel>
     @Override
     protected void initChannel(NioSocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
-        pipeline.addLast(new DelimiterBasedFrameDecoder(2048, delimiter));
         // pipeline.addLast(new DubboRpcEncoder());
+
+        if(COMMON.DUBBO_REQUEST_CONTROL_FLAG){
+            pipeline.addLast(new MyOutBoundHandler());
+        }
+        pipeline.addLast(new DelimiterBasedFrameDecoder(2048, delimiter));
+        if(COMMON.DUBBO_REQUEST_CONTROL_FLAG)
+            pipeline.addLast(new MyInBoundHandler());
         pipeline.addLast(new DubbRpcBackProcess());
     }
 }
