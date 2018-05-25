eb2fa9de582e80c5f2b3c4cc6ae8d5d4b3e620cd
gaoguili <385460398@qq.com>
2018-05-26 00:35:20 +0800

    agent client实现0拷贝

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/registry/EndpointHelper.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/registry/EndpointHelper.java
@@ -29,13 +29,11 @@ public class EndpointHelper {
 
     // 负载均衡算法，最好选择轮转算法，如果采用概率选择算法性能应该会受限
     public static Endpoint getBalancePoint(List<Endpoint> endpoints) {
-        return endpoints.get(random.nextInt(endpoints.size()));
-//        double random = Math.random();
-//        if (random < up_gate) {
+         return endpoints.get(random.nextInt(endpoints.size()));
+//        if (Math.random() < up_gate) {
 //            return endpoints.get(2);
 //        }
 //        return endpoints.get(1);
     }
-
 
 }
